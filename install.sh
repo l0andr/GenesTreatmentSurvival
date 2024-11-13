392deb48e9bac018b99ff9b31d64907183b32602
Andrey Loginov <andreyloginovmob@gmail.com>
2024-11-13 14:22:06 -0500

    Added support for generating plots in TIFF format and updated numpy version in requirements

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pip install -r requirements.txt
+
+git clone https://github.com/l0andr/pysurvtools.git
+pip install -r pysurvtools/requirements.txt
+cp pysurvtools/oncoplot.py .
+cp pysurvtools/survplots.py .
+cp pysurvtools/cox_analysis.py .
+
+git clone https://github.com/l0andr/pyswimplot.git
+pip install -r pyswimplot/requirements.txt
+cp pyswimplot/swimmer_plot.py .
