f76711ae843926a1a422163ca583affd287779a4
Andrey Loginov <andreyloginovmob@gmail.com>
2024-12-04 16:58:57 -0500

    Add adaptree.py to install.sh and update pipeline.sh with new adaptree.py command

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ pip install -r pysurvtools/requirements.txt
 cp pysurvtools/oncoplot.py .
 cp pysurvtools/survplots.py .
 cp pysurvtools/cox_analysis.py .
+cp pysurvtools/adaptree.py .
 
 git clone https://github.com/l0andr/pyswimplot.git
 pip install -r pyswimplot/requirements.txt
